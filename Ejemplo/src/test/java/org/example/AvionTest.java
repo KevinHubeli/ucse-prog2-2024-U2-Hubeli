7b3b1f067548b63f70715742e5c0adc95d2ed297
Kevin_Hubeli <kevin14hubeli@gmail.com>
2024-07-20 18:32:15 -0300

    faltan algunos test

--- Ejemplo/src/test/java/org/example/AvionTest.java
+++ Ejemplo/src/test/java/org/example/AvionTest.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AvionTest {
+
+    @Test
+    void ConsumoDeCombustibleDelAAvion(){
+        Avion avion = new AvionComercial(1000, 50);
+
+        assertEquals(50, avion.getConsumoCombustible());
+    }
+
 }
